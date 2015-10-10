5e955154bb91184745d4c06233db9c3f3ee57854
mrbelowski <jim.britton@yahoo.co.uk>
2015-10-10 21:13:14 +0100

    allow processor affinity to be set

--- CrewChiefV3/Program.cs
+++ CrewChiefV3/Program.cs
@@ -8,12 +8,42 @@ namespace CrewChiefV3
 {
     static class Program
     {
+        private static Dictionary<String, IntPtr> processorAffinities = new Dictionary<String, IntPtr> {
+            { "cpu1", new IntPtr(0x0001) },
+            { "cpu2", new IntPtr(0x0002) },
+            { "cpu3", new IntPtr(0x0004) },
+            { "cpu4", new IntPtr(0x0008) },
+            { "cpu5", new IntPtr(0x0010) },
+            { "cpu6", new IntPtr(0x0020) },
+            { "cpu7", new IntPtr(0x0040) },
+            { "cpu8", new IntPtr(0x0080) }
+        };
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            String[] commandLineArgs = Environment.GetCommandLineArgs();
+            if (commandLineArgs != null)
+            {
+                foreach (String commandLineArg in commandLineArgs)
+                {
+                    if (processorAffinities.ContainsKey(commandLineArg))
+                    {
+                        try
+                        {
+                            var process = System.Diagnostics.Process.GetCurrentProcess();
+                            // Set Core 
+                            process.ProcessorAffinity = processorAffinities[commandLineArg];
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("Failed to set process affinity");
+                        }
+                    }
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainWindow());
