0c64eabc943ddfbd702ee3317035a1999adca5ed
mrbelowski <jim.britton@yahoo.co.uk>
2015-11-10 13:58:21 +0000

    readme

--- CrewChiefV3/CrewChief.cs
+++ CrewChiefV3/CrewChief.cs
@@ -17,7 +17,7 @@ namespace CrewChiefV3
 {
     public class CrewChief : IDisposable
     {
-        public static String Version = "3.8.3";
+        public static String Version = "3.8.4";
 
         public static Boolean loadDataFromFile = false;
 
